8d046fa5bdaba1862266a3dd87f0984c1f8c1ba6
Axiu0823 <69513816+Axiu0823@users.noreply.github.com>
2025-09-23 00:05:07 +0800

    Update rax3000m_256m_diy-part1.sh

--- rax3000m_256m_diy-part1.sh
+++ rax3000m_256m_diy-part1.sh
@@ -14,14 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 
-rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/small/luci-app-mosdns
 rm -rf feeds/small/mosdns
 rm -rf feeds/small/v2ray-geodata
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
